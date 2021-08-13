ed6b37414773a81ae2765e10b63d0c39e15ebb4f
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-08-13 11:40:28 -0700

    Compatibility with upshot-ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document'
+import { InitializeColorMode } from 'theme-ui'
 
 export default class MyDocument extends Document {
   static async getInitialProps(
@@ -19,6 +20,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
+          <InitializeColorMode />
           <Main />
           <NextScript />
         </body>
