f4b3df8ac5e684abd636cd79ffb05a33bb056e3e
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-08-30 17:03:51 -0700

    Dynamic charts

--- src/views/Landing/index.tsx
+++ src/views/Landing/index.tsx
@@ -1,6 +1,16 @@
-import { AppBar, Container, Flex } from '@upshot-tech/upshot-ui'
+import { AppBar, Chart, Container, Flex } from '@upshot-tech/upshot-ui'
 
 export default function LandingView() {
+  const data = [
+    {
+      name: 'Series 1',
+      data: [...new Array(10)].map((_) => Math.floor(Math.random() * 10) + 10),
+    },
+    {
+      name: 'Series 2',
+      data: [...new Array(10)].map((_) => Math.floor(Math.random() * 10) + 10),
+    },
+  ]
   return (
     <Container
       p={4}
@@ -12,7 +22,9 @@ export default function LandingView() {
       }}
     >
       <AppBar />
-      <Flex sx={{ flex: '1 1 auto' }}>[Content]</Flex>
+      <Flex sx={{ flex: '1 1 auto' }}>
+        <Chart {...{ data }} />
+      </Flex>
       [Footer]
     </Container>
   )
