6638e8d344a08a8041f38f901233f76e44eacd7e
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-09-07 15:30:12 -0700

    Explore NFTs

--- src/views/Landing/index.tsx
+++ src/views/Landing/index.tsx
@@ -1,13 +1,27 @@
+import { useBreakpointIndex } from '@theme-ui/match-media'
 import { AppBar, Chart, Container } from '@upshot-tech/upshot-ui'
 import { Box, Flex, MiniNftCard, Text } from '@upshot-tech/upshot-ui'
 import { CollectionButton, Image } from '@upshot-tech/upshot-ui'
+import {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@upshot-tech/upshot-ui'
+import { CollectionRow, CollectionTable } from '@upshot-tech/upshot-ui'
 
 import ButtonTabs from './ButtonTabs'
 import CollectionPanel from './CollectionPanel'
 import { cardItems, chartData, collectionItems } from './constants'
+import ExplorePanel from './ExplorePanel'
 import { MiniNFTContainer } from './Styled'
 
 export default function LandingView() {
+  const breakpointIndex = useBreakpointIndex()
+  const isMobile = breakpointIndex <= 1
+
+  const columns = ['Last Sale', 'Total Sales', '% Change']
+
   return (
     <Container
       p={4}
@@ -57,6 +71,54 @@ export default function LandingView() {
             <MiniNftCard price="$20.00" rarity="15%" key={key} {...{ image }} />
           ))}
         </MiniNFTContainer>
+        <ExplorePanel title="Explore">
+          <CollectionTable>
+            <TableHead>
+              <TableRow>
+                <TableCell colSpan={2}>Name</TableCell>
+                {isMobile ? (
+                  // Mobile only shows the first and last columns
+                  <TableCell>{`${columns[1]} & ${
+                    columns.slice(-1)[0]
+                  }`}</TableCell>
+                ) : (
+                  <>
+                    {columns.map((col, key) => (
+                      <TableCell key={key} sx={{ minWidth: 100 }}>
+                        {col}
+                      </TableCell>
+                    ))}
+                  </>
+                )}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {collectionItems.map(({ text, src }, idx) => (
+                <CollectionRow title={text} imageSrc={src} key={idx}>
+                  {isMobile ? (
+                    <TableCell sx={{ maxWidth: 100 }}>
+                      <Flex
+                        sx={{
+                          flexDirection: 'column',
+                          alignItems: 'flex-end',
+                        }}
+                      >
+                        <Flex>{columns[1]}</Flex>
+                        <Flex>{columns[columns.length - 1]}</Flex>
+                      </Flex>
+                    </TableCell>
+                  ) : (
+                    columns.map((column, key) => (
+                      <TableCell key={key} sx={{ maxWidth: 100 }}>
+                        {column}
+                      </TableCell>
+                    ))
+                  )}
+                </CollectionRow>
+              ))}
+            </TableBody>
+          </CollectionTable>
+        </ExplorePanel>
       </Flex>
       [Footer]
     </Container>
