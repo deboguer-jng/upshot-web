8985ef86b3b19c2fceb240c2e2c02ccfb3a564b1
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2021-11-12 08:43:51 -0800

    Remove priceUSD placeholder

--- src/views/Analytics/components/TopCollectionsChart.tsx
+++ src/views/Analytics/components/TopCollectionsChart.tsx
@@ -111,7 +111,6 @@ export default function TopCollectionsCharts({
         url: `/analytics/collection/${id}`,
         ath: ath ? weiToEth(ath, 2) : null,
         atl: atl ? weiToEth(atl, 2) : null,
-        priceUsd: 10,
         priceChange,
         data: data.map((val, i) =>
           i === 0
