f837a0d533940eea6c10c0fc3d186a8d57af018e
Ethan Bennett <ebennett19@gmail.com>
2021-11-17 20:54:48 -0500

    Fix chart title

--- src/views/Analytics/Collection/index.tsx
+++ src/views/Analytics/Collection/index.tsx
@@ -181,7 +181,7 @@ export default function CollectionView() {
               </Text>
             </Flex>
           </Flex>
-          <Text variant="large" sx={{ textTransform: 'uppercase' }}>
+          <Text variant="large" sx={{ textTransform: 'uppercase', fontWeight: 400 }}>
             General Stats
           </Text>
           <Grid columns="repeat(auto-fit, minmax(100px, 1fr))" sx={{ gap: 4 }}>
@@ -211,8 +211,11 @@ export default function CollectionView() {
           </Text>
         </Flex>
       </Grid>
-      <Text variant="large" sx={{ textTransform: 'uppercase' }}>
-        {name} sales this month
+      <Text variant="large" sx={{ textTransform: 'uppercase', fontWeight: 400, marginTop: '20px' }}>
+        {name}
+      </Text>
+      <Text variant="h2Primary" sx={{ textTransform: 'uppercase', fontWeight: 400, marginTop: '-15px' }}>
+        sales this month
       </Text>
       {scatterChart}
 
