2592d037e6009a8cab178ef870bfebf404e18cf6
DaWe <willteam35@gmail.com>
2021-11-27 15:59:09 +0100

    new landing page

--- src/views/Landing/index.tsx
+++ src/views/Landing/index.tsx
@@ -1,13 +1,16 @@
 import {
   Box,
+  Button,
   Container,
   Flex,
   Footer,
   Grid,
   Icon,
+  IconButton,
   LandingPanel,
   Panel,
   Text,
+  theme,
 } from '@upshot-tech/upshot-ui'
 import { Nav } from 'components/Nav'
 import Head from 'next/head'
@@ -15,7 +18,9 @@ import { Image, Link } from 'theme-ui'
 
 import { projects } from './content'
 import AnalyticsSVG from './panelBackgrounds/Analytics.svg'
-import UpshotOneSVG from './panelBackgrounds/UpshotOne.svg'
+import BlogSVG from './panelBackgrounds/Blog.svg'
+import UpshotApiSVG from './panelBackgrounds/UpshotApi.svg'
+import UpshotArtworkSVG from './panelBackgrounds/UpshotArtwork.svg'
 
 type PanelData = {
   projectType: string
@@ -79,42 +84,82 @@ export default function LandingView() {
         }}
       >
         <Nav />
-        <Box sx={{ display: ['none', 'none', 'none', 'flex'] }}>
+        <Box sx={{ display: 'flex' }}>
           {' '}
           {/* Display only on Desktop */}
-          <Flex sx={{ gap: 15, marginTop: '5%', marginBottom: '5%' }}>
-            <Icon color="primary" icon="bubblegum" size={240} />
+          <Grid columns={[1, null, '3fr 2fr']} sx={{ marginTop: '5%', marginBottom: '2%' }}>
             <Box>
               <Box>
-                <Box>
-                  <Text variant="h0Primary" sx={{ fontSize: '50px' }}>
-                    Upshot: Where NFTs meet DeFi
-                  </Text>
-                </Box>
-                <Box sx={{ marginTop: '30px' }}>
-                  <Text
-                    variant="h3Primary"
-                    sx={{ fontWeight: 'normal', lineHeight: '1.3em' }}
-                  >
-                    NFTs offer us a vehicle for tokenizing anything, while the
-                    explosive growth of DeFi has demonstrated the power of
-                    permissionless financial primitives. Upshot is building
-                    scalable NFT pricing infrastructure at the intersection of
-                    DeFi x NFTs. Through a combination of crowdsourced
-                    appraisals and proprietary machine learning algorithms,
-                    Upshot provides deep insight into NFT markets and unlocks a
-                    wave of exotic new DeFi possibilities.
-                  </Text>
-                </Box>
+                <Text variant="h0Primary" sx={{ fontSize: ['3rem', null, '5rem', '8rem'], textTransform: 'uppercase' }}>
+                  Upshot
+                </Text>
               </Box>
+              <Box sx={{ marginTop: ['0', null, '0', '1rem'] }}>
+                <Text variant="h1Primary" sx={{ fontSize: ['1.15rem', null, '1.8rem', '2.8rem'] }}>
+                  Where NFTs meet DeFi
+                </Text>
+              </Box>
+              <Box sx={{ marginTop: ['0', null, '0.6rem', '1rem'] }}>
+                <Text
+                  variant="h3Primary"
+                  sx={{ fontWeight: 'normal', lineHeight: ['1.25rem', null, '1.375em', '1.75rem'] }}
+                >
+                  Upshot provides deep insight into NFT markets and unlocks a wave of exotic new DeFi possibilities.
+                </Text>
+              </Box>
+              <Flex sx={{ gridGap: '14px', marginTop: '20px' }}>
+                <Box>
+                  <Link href="https://discord.gg/upshot" target="_blank">
+                    <IconButton>
+                      <Icon color="purple" icon="discord" size={32} />
+                    </IconButton>
+                  </Link>
+                </Box>
+                <Box>
+                  <Link href="https://twitter.com/upshothq" target="_blank">
+                    <IconButton>
+                      <Icon color="blue" icon="twitterCircle" size={32} />
+                    </IconButton>
+                  </Link>
+                </Box>
+                <Box>
+                  <Link
+                    href="https://jobs.lever.co/upshot.io"
+                    target="_blank"
+                    sx={{
+                      textDecoration: 'none',
+                      '&:hover': {
+                        textDecoration: 'none',
+                      }
+                    }}>
+                      <Button capitalize={true} style={{
+                          borderRadius: '8px',
+                          height: '32px',
+                          padding: '8px 12px',
+                      }}>
+                        Join our team
+                      </Button>
+                  </Link>
+                </Box>
+              </Flex>
             </Box>
-          </Flex>
+            
+            <Box sx={{ marginTop: ['-20px', null, '-40px', '-80px'] }}>
+              <Image
+                src={UpshotArtworkSVG.src}
+                sx={{
+                  width: "100%",
+                }}
+                alt="Upshot"
+              />
+            </Box>
+          </Grid>
         </Box>
         <Text variant="h2Primary">Get started</Text>
         <Text variant="xLarge" color="grey-500">
-          Gain insight into NFT markets with our suite of tools.
+          Start using our suite of tools for exploring the metaverse.
         </Text>
-        <Grid gap={5} columns={[1, 1, 2, 3]}>
+        <Grid gap={5} columns={[1, 1, 2, '2fr 1fr 1fr']}>
           <Link
             href="/analytics"
             rel="noopener noreferrer"
@@ -130,11 +175,12 @@ export default function LandingView() {
             <Panel
               hoverUnderglow="pink"
               sx={{
+                height: ['225px', null, null, '250px'],
                 position: 'relative',
-                padding: '32px !important',
+                padding: '22px !important',
                 paddingRight: '88px !important',
-                background:
-                  'linear-gradient(229.88deg, rgba(236, 91, 148, 0.18) 14.66%, rgba(236, 91, 148, 0.18) 14.66%, rgba(236, 91, 148, 0) 100.35%), #231F20',
+                paddingTop: '10px !important',
+                backgroundColor: theme.colors['grey-900'] + ' !important',
                 '&:hover img': {
                   opacity: '1',
                 },
@@ -143,20 +189,20 @@ export default function LandingView() {
               <Image
                 src={AnalyticsSVG.src}
                 sx={{
-                  opacity: '0.3',
+                  opacity: '0.7',
                   position: 'absolute',
-                  left: '0',
-                  top: '0',
-                  width: '100%',
-                  height: 'auto',
+                  right: '5%',
+                  bottom: '0',
+                  width: '70%',
                   objectFit: 'cover',
                 }}
+                alt="Analytics"
               ></Image>
               <div style={{ position: 'relative' }}>
                 <Text variant="h1Secondary">Analytics</Text>
                 <Text
                   variant="large"
-                  sx={{ paddingTop: '60px', display: 'block' }}
+                  sx={{ display: 'block', width: ['100%', null, null, '50%'], }}
                 >
                   Discover the world of NFTs using powerful data.
                 </Text>
@@ -178,137 +224,94 @@ export default function LandingView() {
             <Panel
               hoverUnderglow="blue"
               sx={{
+                height: ['225px', null, null, '250px'],
                 position: 'relative',
-                padding: '32px !important',
+                padding: '22px !important',
                 paddingRight: '88px !important',
-                background:
-                  'linear-gradient(229.88deg, rgba(0, 145, 255, 0.3) 14.66%, rgba(0, 145, 255, 0) 100.35%), #231F20',
+                paddingTop: '10px !important',
+                backgroundColor: theme.colors['grey-900'] + ' !important',
                 '&:hover img': {
                   opacity: '1',
                 },
               }}
             >
               <Image
-                src={UpshotOneSVG.src}
+                src={UpshotApiSVG.src}
                 sx={{
-                  opacity: '0.3',
+                  opacity: '0.7',
                   position: 'absolute',
-                  right: '0',
-                  top: '0',
+                  bottom: '5%',
+                  left: '50%',
+                  transform: 'translateX(-50%)',
                   width: 'auto',
-                  height: '50%',
+                  height: '60%',
                   objectFit: 'cover',
                 }}
+                alt="Upshot One"
               ></Image>
               <div style={{ position: 'relative' }}>
                 <Text variant="h1Secondary">Upshot API</Text>
                 <Text
                   variant="large"
-                  sx={{ paddingTop: '60px', display: 'block' }}
+                  sx={{ display: 'block' }}
                 >
                   Integrate Upshot data into your project.
                 </Text>
               </div>
             </Panel>
           </Link>
-          <Grid
-            gap={2}
-            columns={[1, 1, 2]}
+          <Link
+            href="https://blog.upshot.io/"
+            rel="noopener noreferrer"
             sx={{
-              padding: '18px',
-              border: '1px solid #545454',
-              borderRadius: '24px',
+              color: 'text',
+              textDecoration: 'none',
+              display: 'grid',
+              '&:hover': {
+                textDecoration: 'none',
+              },
             }}
           >
-            <Text
-              color="grey-600"
+            <Panel
+              hoverUnderglow="blue"
               sx={{
-                position: 'absolute',
-                transform: 'translate(0%, -150%)',
-                bg: 'black',
-                paddingLeft: '10px',
-                paddingRight: '10px',
-              }}
-            >
-              Additional Resources
-            </Text>
-            <Link
-              href={'https://blog.upshot.io'}
-              target="_blank"
-              sx={{
-                color: 'initial',
-                textDecoration: 'none',
-                display: 'grid',
-                '&:hover': {
-                  textDecoration: 'none',
+                height: ['225px', null, null, '250px'],
+                position: 'relative',
+                padding: '22px !important',
+                paddingRight: '88px !important',
+                paddingTop: '10px !important',
+                backgroundColor: theme.colors['grey-900'] + ' !important',
+                '&:hover img': {
+                  opacity: '1',
                 },
               }}
             >
-              <LandingPanel
-                title="Blog"
-                description="Check out our blog"
-                showLinkIcon={true}
-                hoverUnderglow="blue"
-              />
-            </Link>
-            <Link
-              href={'https://jobs.lever.co/upshot.io'}
-              target="_blank"
-              sx={{
-                color: 'initial',
-                textDecoration: 'none',
-                display: 'grid',
-                '&:hover': {
-                  textDecoration: 'none',
-                },
-              }}
-            >
-              <LandingPanel
-                title="Careers"
-                description="Join our team"
-                showLinkIcon={true}
-                hoverUnderglow="blue"
-              />
-            </Link>
-            <Link
-              href={'https://discord.gg/upshot'}
-              target="_blank"
-              sx={{
-                color: 'initial',
-                textDecoration: 'none',
-                display: 'grid',
-                '&:hover': {
-                  textDecoration: 'none',
-                },
-              }}
-            >
-              <LandingPanel
-                title="Discord"
-                description="Become a part of the community"
-                showLinkIcon={true}
-                hoverUnderglow="blue"
-              />
-            </Link>
-            <Link
-              href={'https://twitter.com/UpshotHQ'}
-              target="_blank"
-              sx={{
-                color: 'initial',
-                textDecoration: 'none',
-                display: 'grid',
-                '&:hover': {
-                  textDecoration: 'none',
-                },
-              }}
-            >
-              <LandingPanel
-                title="Twitter"
-                description="Follow us on Twitter"
-                showLinkIcon={true}
-                hoverUnderglow="blue"
-              />
-            </Link>
-          </Grid>
+              <Image
+                src={BlogSVG.src}
+                sx={{
+                  opacity: '0.7',
+                  position: 'absolute',
+                  bottom: '5%',
+                  left: '50%',
+                  transform: 'translateX(-50%)',
+                  margin: 'auto',
+                  width: 'auto',
+                  height: '50%',
+                  objectFit: 'cover',
+                }}
+                alt="Upshot One"
+              ></Image>
+              <div style={{ position: 'relative' }}>
+                <Text variant="h1Secondary">Blog</Text>
+                <Text
+                  variant="large"
+                  sx={{ display: 'block' }}
+                >
+                  Learn more about NFTxDeFI
+                </Text>
+              </div>
+            </Panel>
+          </Link>
         </Grid>
 
         <Text variant="h2Primary">Discover</Text>
