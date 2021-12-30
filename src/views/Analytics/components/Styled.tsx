8ef8f1f220302dda43c9d2bdbc308a37cdc00834
devbiran <ib660987@gmail.com>
2021-12-30 14:18:37 +0100

    top selling nfts improved.

--- src/views/Analytics/components/Styled.tsx
+++ src/views/Analytics/components/Styled.tsx
@@ -2,13 +2,16 @@ import styled from '@emotion/styled'
 import { Box } from '@upshot-tech/upshot-ui'
 
 export const MiniNFTContainer = styled(Box)`
-  ${({ theme: { scroll } }) => scroll.thin}
-
   display: flex;
   margin-bottom: ${({ theme }) => theme.sizes[3] + 'px'};
   gap: ${({ theme }) => theme.sizes[7] + 'px'};
   padding-bottom: ${({ theme }) => theme.sizes[5] + 'px'};
-  overflow-x: auto;
+  overflow: hidden;
+  
+  &:hover {
+    overflow-x: auto;
+    ${({ theme: { scroll } }) => scroll.thin}
+  }
 
   &::after {
     display: block;
