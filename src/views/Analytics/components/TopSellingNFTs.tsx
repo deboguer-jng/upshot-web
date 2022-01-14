c2b9e19f973d6ae17cadb2e04f61914abaf39a2b
Ethan Bennett <ebennett19@gmail.com>
2022-01-14 00:04:58 -0500

    Adjust top selling nfts gradient & collector accordion font

--- src/views/Analytics/components/TopSellingNFTs.tsx
+++ src/views/Analytics/components/TopSellingNFTs.tsx
@@ -178,7 +178,7 @@ export default function TopSellingNFTs({
             background: 'black',
             height: 'calc(100% - 80px)',
             WebkitMaskImage:
-              'linear-gradient(to right, rgba(0, 0, 0, 0) 70%, rgba(0,0,0,1) 100%);',
+              'linear-gradient(to right, rgba(0, 0, 0, 0) 65%, rgba(0,0,0,1) 100%);',
             zIndex: 2,
             pointerEvents: 'none',
           }}
