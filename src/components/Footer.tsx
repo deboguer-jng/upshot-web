baee2020fb8c70a8b8f5ffa7eeed24053854b016
DaWe <willteam35@gmail.com>
2022-01-21 21:05:57 +0100

    Fix Footer type error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,8 @@
-import { Footer as FooterUI } from '@upshot-tech/upshot-ui'
+import { Footer as FooterUI, FooterProps } from '@upshot-tech/upshot-ui'
 import { useAppSelector } from 'redux/hooks'
 import { selectShowSidebar } from 'redux/reducers/layout'
-import { FlexProps } from 'theme-ui'
 
-export const Footer = (props: FlexProps) => {
+export const Footer = (props: FooterProps) => {
   const showSidebar = useAppSelector(selectShowSidebar)
 
   return <FooterUI sidebarVisible={showSidebar} {...props} />
