40c8e84f65712e0182fa61cbb2e3060a02305fc4
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-02-17 12:23:57 -0800

    Avoid merging over user fields

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -4,7 +4,20 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
  * Creates a new Apollo client instance.
  */
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          getUser: {
+            keyArgs: ['id', 'userAddress'],
+            merge(existing = {}, incoming) {
+              return { ...existing, ...incoming }
+            },
+          },
+        },
+      },
+    },
+  }),
   uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
 })
 
