e763a6950b01f74afc026c3da41774fe8bfd06c3
nickemmons <nicholas.r.emmons@gmail.com>
2022-03-02 08:52:35 -0500

    make sure rarity shows up even when asset is unappraised

--- src/views/Analytics/NFT/index.tsx
+++ src/views/Analytics/NFT/index.tsx
@@ -232,6 +232,9 @@ export default function NFTView() {
     ? image
     : optimizedSrc
 
+  console.log('rarity', rarityRank)
+  console.log('collection', collection)
+
   return (
     <>
       <Head>
@@ -315,13 +318,15 @@ export default function NFTView() {
             )}
             <Flex sx={{ flexDirection: 'column', gap: 4 }}>
               <Text variant="h2Primary">{assetName}</Text>
-              {!!latestAppraisal && (
                 <>
                   <Flex sx={{ alignItems: 'center', gap: 2, flexWrap: 'wrap' }}>
+                  {!!latestAppraisal && (
                     <Label size="md" color="blue">
                       {'Last Appraisal: Ξ' +
                         weiToEth(latestAppraisal.ethSalePrice, 3, false)}
                     </Label>
+
+                  )}
 
                     {!!rarityRank && !!collection && !!collection?.size && (
                       <Label size="md">
@@ -329,6 +334,7 @@ export default function NFTView() {
                       </Label>
                     )}
                   </Flex>
+                  {!!latestAppraisal && (
                   <a
                     href="https://mirror.xyz/0x82FE4757D134a56BFC7968A0f0d1635345053104"
                     target="_blank"
@@ -354,8 +360,8 @@ export default function NFTView() {
                       How did we calculate this appraisal?
                     </Box>
                   </a>
+                  )}
                 </>
-              )}
 
               <Flex>
                 <a
