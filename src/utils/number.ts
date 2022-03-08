b2458d5913e9d2327f0215dddcc4e34cd0d7704a
nickemmons <nicholas.r.emmons@gmail.com>
2022-03-08 13:50:35 -0500

    fix comment on underoverpriced label

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -92,7 +92,7 @@ export const getPriceChangeLabel = (val: number | null) => {
 /**
  * Get underpriced/overpriced label.
  *
- * @returns + prefixed percent if positive, - prefixed percent if negative.
+ * @returns (underpriced) appended if underpriced, - (overpriced) appended if overpriced.
  */
  export const getUnderOverPricedLabel = (val: number | null) => {
   if (val === undefined || val === null) return '-'
