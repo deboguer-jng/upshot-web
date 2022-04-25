121a52bf52491d4e539db7d7183df71cbf83426f
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-04-25 15:11:17 -0700

    Log events (auth / view)

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,1 +1,42 @@
-export {}
+import { gql } from '@apollo/client'
+
+/**
+ * Log Event
+ * @see views/Layout
+ */
+export type LogEventVars = {
+  timestamp: number
+  address: string
+  type: string
+  value?: string
+}
+
+export type LogEventData = {
+  logEvent: {
+    type: string
+    value: string
+    address: string
+    timestamp: number
+  }
+}
+
+export const LOG_EVENT = gql`
+  mutation LogEvent(
+    $timestamp: Int!
+    $address: String!
+    $type: String!
+    $value: String
+  ) {
+    logEvent(
+      timestamp: $timestamp
+      address: $address
+      type: $type
+      value: $value
+    ) {
+      type
+      value
+      address
+      timestamp
+    }
+  }
+`
