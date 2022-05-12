9571578d31d8a275208448a508be5a3b8063d8fa
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-05-12 15:59:49 -0700

    Inline meta

--- src/views/Gmi.tsx
+++ src/views/Gmi.tsx
@@ -650,17 +650,24 @@ export default function GmiView() {
         <title>gmi | Upshot Analytics</title>
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@UpshotHQ" />
-        <meta name="twitter:creator" content="@UpshotHQ" />
-        <meta property="og:url" content="https://upshot.io" />
-        <meta property="og:title" content="Upshot Analytics" />
         <meta
-          property="og:description"
-          content="NFTs offer us a vehicle for tokenizing anything, while the explosive growth of DeFi has demonstrated the power of permissionless financial primitives. Upshot is building scalable NFT pricing infrastructure at the intersection of DeFi x NFTs. Through a combination of crowdsourced appraisals and proprietary machine learning algorithms, Upshot provides deep insight into NFT markets and unlocks a wave of exotic new DeFi possibilities."
+          name="twitter:title"
+          content={
+            router.query['wallet']
+              ? `${router.query['wallet']} | Upshot gmi`
+              : 'Upshot gmi'
+          }
         />
         <meta
-          property="og:image"
-          content="https://upshot.io/img/opengraph/opengraph_analytics.jpg"
+          name="twitter:description"
+          content="Search any wallet to see the Upshot gmi."
         />
+        {!!router.query['wallet'] && (
+          <meta
+            name="twitter:image"
+            content={`https://stage.analytics.upshot.io/.netlify/functions/gmi?wallet=${router.query['wallet']}&filetype=.png`}
+          />
+        )}
       </Head>
       <Box
         sx={{
