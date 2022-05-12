4e317d00c17ac716cb5b42ea528843109596ba36
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-05-12 15:44:18 -0700

    puppeteer configuration

--- netlify/functions/gmi.js
+++ netlify/functions/gmi.js
@@ -1,8 +1,52 @@
-exports.handler = async function (event, context) {
-  console.log({ body: event.body, qs: event.queryStringParameters })
+const chromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Success' }),
+const config = {
+  width: 1280,
+  height: 640,
+  port: 3001,
+  maxAge: 60 * 60,
+  baseUrl: 'https://stage.analytics.upshot.io/share/gmi/',
+}
+
+exports.handler = async ({ queryStringParameters: qs }) => {
+  if (!qs.wallet)
+    return {
+      statusCode: 500,
+    }
+
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: { width: config.width, height: config.height },
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  })
+
+  const page = await browser.newPage()
+
+  try {
+    const result = await page.goto(url)
+
+    await page.evaluateHandle('document.fonts.ready')
+    await page.waitForSelector('#gmiResults')
+
+    if (result.ok()) {
+      const buffer = await page.screenshot()
+
+      return {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'image/png',
+        },
+        body: buffer.toString('base64'),
+        isBase64Encoded: true,
+      }
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+    }
   }
+
+  await browser.close()
 }
