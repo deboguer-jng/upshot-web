9685f8c31c1205108f2e57157564a64f0c36f736
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2022-05-31 14:09:04 -0700

    Rm unused code

--- src/views/Analytics/Collection/index.tsx
+++ src/views/Analytics/Collection/index.tsx
@@ -1,30 +1,16 @@
 /** @jsxImportSource theme-ui */
 import { useQuery } from '@apollo/client'
-import {
-  imageOptimizer,
-  Link,
-  theme,
-  Tooltip,
-  useBreakpointIndex,
-} from '@upshot-tech/upshot-ui'
+import { theme, useBreakpointIndex } from '@upshot-tech/upshot-ui'
 import { Container, Flex, Grid } from '@upshot-tech/upshot-ui'
-import {
-  Avatar,
-  Button,
-  formatNumber,
-  Icon,
-  Text,
-} from '@upshot-tech/upshot-ui'
+import { formatNumber, Text } from '@upshot-tech/upshot-ui'
 import { Footer } from 'components/Footer'
 import { Nav } from 'components/Nav'
-import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 import React, { useEffect, useMemo, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { useSelector } from 'react-redux'
 import { useAppDispatch } from 'redux/hooks'
 import { selectShowHelpModal, setShowHelpModal } from 'redux/reducers/layout'
-import { Box } from 'theme-ui'
 import CollectionScatterChart from 'views/Analytics/components/CollectionScatterChart'
 import ExplorePanel from 'views/Analytics/components/ExplorePanel'
 import TopSellingNFTs from 'views/Analytics/components/TopSellingNFTs'
@@ -70,13 +56,7 @@ function CollectionStat({
   )
 }
 
-function Layout({
-  children,
-  title,
-}: {
-  children: React.ReactNode
-  title?: string
-}) {
+function Layout({ children }: { children: React.ReactNode }) {
   const storage = globalThis?.sessionStorage
   const prevPath = storage.getItem('prevPath')
 
@@ -166,209 +146,204 @@ export default function CollectionView() {
     [id, data]
   )
 
-  /* Load state. */
-
-  const loadLayout = useMemo(
+  const loadContent = useMemo(
     () => (
-      <Layout>
-        <CollectionHeader key={id} />
-
-        <Flex sx={{ flexDirection: 'column', gap: 4 }}>
-          <img src="/img/Logo_bounce_spin.gif" width={256} alt="Loading" />
-        </Flex>
-      </Layout>
+      <Flex sx={{ flexDirection: 'column', gap: 4 }}>
+        <img src="/img/Logo_bounce_spin.gif" width={256} alt="Loading" />
+      </Flex>
     ),
     []
   )
 
-  const emptyLayout = useMemo(
+  const emptyContent = useMemo(
     () => (
-      <Layout>
-        <CollectionHeader />
-
-        <Container sx={{ justifyContent: 'center' }}>
-          Unable to load collection.
-        </Container>
-      </Layout>
+      <Container sx={{ justifyContent: 'center' }}>
+        Unable to load collection.
+      </Container>
     ),
     []
   )
 
-  return loading ? (
-    loadLayout
-  ) : !data?.collectionById ? (
-    emptyLayout
-  ) : (
-    <Layout title={data.collectionById.name}>
+  return (
+    <Layout>
       <CollectionHeader />
 
-      <Flex
-        sx={{
-          flexDirection: 'column',
-          gap: 4,
-          maxWidth: '1280px',
-          marginX: 'auto',
-        }}
-      >
-        <Grid columns={['1fr', '1fr', '1fr 1fr']} sx={{ gap: '40px' }}>
-          <Flex sx={{ flexDirection: 'column', gap: '16px' }}>
-            <Text
-              variant="large"
-              sx={{ textTransform: 'uppercase', fontWeight: 400 }}
-            >
-              General Stats
-            </Text>
-            <Grid
-              columns="repeat(auto-fit, minmax(140px, 1fr))"
-              sx={{ gap: 4 }}
-            >
-              <CollectionStat
-                color="blue"
-                value={
-                  data.collectionById?.latestStats?.average
-                    ? formatNumber(data.collectionById?.latestStats?.average, {
-                        fromWei: true,
-                        decimals: 2,
-                        prefix: 'ETHER',
-                      })
-                    : '-'
-                }
-                label="Average Price"
-              />
-              <CollectionStat
-                color="pink"
-                value={
-                  data.collectionById?.latestStats?.floor
-                    ? formatNumber(data.collectionById?.latestStats.floor, {
-                        fromWei: true,
-                        decimals: 2,
-                        prefix: 'ETHER',
-                      })
-                    : '-'
-                }
-                label="Floor Price"
-              />
-              <CollectionStat
-                color={
-                  data.collectionById.latestStats?.weekFloorChange
-                    ? data.collectionById.latestStats?.weekFloorChange > 0
-                      ? 'green'
-                      : 'red'
-                    : 'white'
-                }
-                value={
-                  data.collectionById.latestStats?.weekFloorChange
-                    ? data.collectionById.latestStats?.weekFloorChange > 0
-                      ? '+' +
-                        data.collectionById.latestStats?.weekFloorChange.toFixed(
-                          2
-                        ) +
-                        '%'
-                      : data.collectionById.latestStats?.weekFloorChange.toFixed(
-                          2
-                        ) + '%'
-                    : '-'
-                }
-                label="7 Day Floor Change"
-              />
-              <CollectionStat
-                value={
-                  data.collectionById?.latestStats?.floorCap
-                    ? formatNumber(data.collectionById.latestStats.floorCap, {
-                        fromWei: true,
-                        decimals: 2,
-                        kmbUnits: true,
-                        prefix: 'ETHER',
-                      })
-                    : '-'
-                }
-                label="Floor Cap"
-              />
-              <CollectionStat
-                value={
-                  data.collectionById?.latestStats?.pastWeekWeiVolume
-                    ? formatNumber(
-                        data.collectionById.latestStats.pastWeekWeiVolume,
-                        {
+      {loading ? (
+        loadContent
+      ) : !data?.collectionById ? (
+        emptyContent
+      ) : (
+        <Flex
+          sx={{
+            flexDirection: 'column',
+            gap: 4,
+            maxWidth: '1280px',
+            marginX: 'auto',
+          }}
+        >
+          <Grid columns={['1fr', '1fr', '1fr 1fr']} sx={{ gap: '40px' }}>
+            <Flex sx={{ flexDirection: 'column', gap: '16px' }}>
+              <Text
+                variant="large"
+                sx={{ textTransform: 'uppercase', fontWeight: 400 }}
+              >
+                General Stats
+              </Text>
+              <Grid
+                columns="repeat(auto-fit, minmax(140px, 1fr))"
+                sx={{ gap: 4 }}
+              >
+                <CollectionStat
+                  color="blue"
+                  value={
+                    data.collectionById?.latestStats?.average
+                      ? formatNumber(
+                          data.collectionById?.latestStats?.average,
+                          {
+                            fromWei: true,
+                            decimals: 2,
+                            prefix: 'ETHER',
+                          }
+                        )
+                      : '-'
+                  }
+                  label="Average Price"
+                />
+                <CollectionStat
+                  color="pink"
+                  value={
+                    data.collectionById?.latestStats?.floor
+                      ? formatNumber(data.collectionById?.latestStats.floor, {
+                          fromWei: true,
+                          decimals: 2,
+                          prefix: 'ETHER',
+                        })
+                      : '-'
+                  }
+                  label="Floor Price"
+                />
+                <CollectionStat
+                  color={
+                    data.collectionById.latestStats?.weekFloorChange
+                      ? data.collectionById.latestStats?.weekFloorChange > 0
+                        ? 'green'
+                        : 'red'
+                      : 'white'
+                  }
+                  value={
+                    data.collectionById.latestStats?.weekFloorChange
+                      ? data.collectionById.latestStats?.weekFloorChange > 0
+                        ? '+' +
+                          data.collectionById.latestStats?.weekFloorChange.toFixed(
+                            2
+                          ) +
+                          '%'
+                        : data.collectionById.latestStats?.weekFloorChange.toFixed(
+                            2
+                          ) + '%'
+                      : '-'
+                  }
+                  label="7 Day Floor Change"
+                />
+                <CollectionStat
+                  value={
+                    data.collectionById?.latestStats?.floorCap
+                      ? formatNumber(data.collectionById.latestStats.floorCap, {
                           fromWei: true,
                           decimals: 2,
                           kmbUnits: true,
                           prefix: 'ETHER',
-                        }
-                      )
-                    : '-'
-                }
-                label="Weekly Volume"
-              />
-              <CollectionStat
-                value={
-                  data.collectionById?.size
-                    ? formatNumber(data.collectionById.size)
-                    : '-'
-                }
-                label="NFTs in Collection"
-              />
-            </Grid>
-          </Flex>
-          <Flex
+                        })
+                      : '-'
+                  }
+                  label="Floor Cap"
+                />
+                <CollectionStat
+                  value={
+                    data.collectionById?.latestStats?.pastWeekWeiVolume
+                      ? formatNumber(
+                          data.collectionById.latestStats.pastWeekWeiVolume,
+                          {
+                            fromWei: true,
+                            decimals: 2,
+                            kmbUnits: true,
+                            prefix: 'ETHER',
+                          }
+                        )
+                      : '-'
+                  }
+                  label="Weekly Volume"
+                />
+                <CollectionStat
+                  value={
+                    data.collectionById?.size
+                      ? formatNumber(data.collectionById.size)
+                      : '-'
+                  }
+                  label="NFTs in Collection"
+                />
+              </Grid>
+            </Flex>
+            <Flex
+              sx={{
+                flexDirection: 'column',
+                gap: '16px',
+              }}
+            >
+              {data.collectionById?.description && (
+                <Text variant="large" sx={{ textTransform: 'uppercase' }}>
+                  About
+                </Text>
+              )}
+              <DescriptionWrapper color="grey-300">
+                {(
+                  <ReactMarkdown allowedElements={['a', 'p']}>
+                    {data.collectionById.description}
+                  </ReactMarkdown>
+                ) ?? 'No information.'}
+              </DescriptionWrapper>
+            </Flex>
+          </Grid>
+          <Text
+            variant="large"
             sx={{
-              flexDirection: 'column',
-              gap: '16px',
+              textTransform: 'uppercase',
+              fontWeight: 400,
+              marginTop: '20px',
             }}
           >
-            {data.collectionById?.description && (
-              <Text variant="large" sx={{ textTransform: 'uppercase' }}>
-                About
-              </Text>
-            )}
-            <DescriptionWrapper color="grey-300">
-              {(
-                <ReactMarkdown allowedElements={['a', 'p']}>
-                  {data.collectionById.description}
-                </ReactMarkdown>
-              ) ?? 'No information.'}
-            </DescriptionWrapper>
+            {data.collectionById?.name}
+          </Text>
+          <Text
+            variant="h2Primary"
+            sx={{
+              textTransform: 'uppercase',
+              fontWeight: 400,
+              marginTop: '-15px',
+            }}
+          >
+            sales this month
+          </Text>
+          {scatterChart}
+
+          <Flex
+            sx={{
+              position: 'relative',
+              flexDirection: 'column',
+              flexGrow: 1,
+              gap: 5,
+            }}
+          >
+            <TopSellingNFTs collectionId={id} />
           </Flex>
-        </Grid>
-        <Text
-          variant="large"
-          sx={{
-            textTransform: 'uppercase',
-            fontWeight: 400,
-            marginTop: '20px',
-          }}
-        >
-          {data.collectionById?.name}
-        </Text>
-        <Text
-          variant="h2Primary"
-          sx={{
-            textTransform: 'uppercase',
-            fontWeight: 400,
-            marginTop: '-15px',
-          }}
-        >
-          sales this month
-        </Text>
-        {scatterChart}
 
-        <Flex
-          sx={{
-            position: 'relative',
-            flexDirection: 'column',
-            flexGrow: 1,
-            gap: 5,
-          }}
-        >
-          <TopSellingNFTs collectionId={id} />
+          <ExplorePanel
+            collectionId={id}
+            collectionName={data.collectionById?.name}
+            isAppraised={data.collectionById?.isAppraised}
+          />
         </Flex>
-
-        <ExplorePanel
-          collectionId={id}
-          collectionName={data.collectionById?.name}
-          isAppraised={data.collectionById?.isAppraised}
-        />
-      </Flex>
+      )}
     </Layout>
   )
 }
